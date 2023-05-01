07e64def2a43a361d935dab880eb8696f5966a73
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2023-05-01 17:01:21 -0300

    fix: formatted date

--- Sources/DateFormatting/DateFormatting.swift
+++ Sources/DateFormatting/DateFormatting.swift
@@ -4,14 +4,19 @@ import Foundation
 public struct DateFormatting {
 
     public static func main() {
-        let date = DateFormatting().dateFromString(dateString: "01/01/2022")
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        let date = formatter.date(from: "01/01/2022")
 
         print(DateFormatting().formatDate(date: date!))
     }
 
     func formatDate(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        let dateFormatted = dateFormatter.string(from: date)
 
-        return date.formatted()
+        return dateFormatted
     }
 
     func dateFromString(dateString: String) -> Date? {
